f47c6511a407b0af692771de61019d9ab953ddc2
Thanmaikishore12 <thanmaikishore@gmail.com>
2026-07-23 21:23:18 +0530

    Add files via upload

---
+++ DeepSkilling/Module-08-Angular/excersice06/course-summary-widget.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseService } from '../../services/course';
+
+@Component({
+  selector: 'app-course-summary-widget',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-summary-widget.html',
+  styleUrl: './course-summary-widget.css'
+})
+export class CourseSummaryWidget implements OnInit {
+
+  totalCourses = 0;
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.totalCourses = this.courseService.getCourses().length;
+  }
+
+}
