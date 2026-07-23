aca259ce44ca31d40bbedaa549f57caf1b400bfc
Thanmaikishore12 <thanmaikishore@gmail.com>
2026-07-23 19:40:57 +0530

    Add files via upload

---
+++ DeepSkilling/Module-08-Angular/excersice02/components/course-card/course-card.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css'
+})
+export class CourseCard implements OnChanges {
+
+  @Input() course!: {
+    id: number;
+    name: string;
+    code: string;
+    credits: number;
+  };
+
+  @Output() enrollRequested = new EventEmitter<number>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Course changed:', changes['course']);
+  }
+
+}
---
+++ DeepSkilling/Module-08-Angular/excersice02/pages/course-list/course-list.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [CommonModule, CourseCard],
+  templateUrl: './course-list.html',
+  styleUrl: './course-list.css'
+})
+export class CourseList {
+
+  courses = [
+    { id: 1, name: 'Angular', code: 'ANG101', credits: 4 },
+    { id: 2, name: 'Java', code: 'JAVA201', credits: 3 },
+    { id: 3, name: 'SQL', code: 'SQL301', credits: 3 },
+    { id: 4, name: 'Python', code: 'PY401', credits: 4 },
+    { id: 5, name: 'C#', code: 'CS501', credits: 4 }
+  ];
+
+  selectedCourseId: number | null = null;
+
+  onEnroll(courseId: number): void {
+    console.log('Enrolling in course: ' + courseId);
+    this.selectedCourseId = courseId;
+  }
+
+}
